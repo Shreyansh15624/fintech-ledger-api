f2754045a94729bc7a371910a7b43252aa3ba1d6
Shreyansh15624 <shreyanshvajroju@gmail.com>
2026-04-05 21:10:04 +0530

    Improved on Documentation for better Understanding

--- app/main.py
+++ app/main.py
@@ -7,20 +7,41 @@ from app.routers import auth, records, analytics, users
 # all the tables based of the 'models.py' file
 models.Base.metadata.create_all(bind=engine)
 
-# 2. The Engine: Initializing the FastAPI Application
+# 2. Documentation: Defining the new Documentation Metadata for our Tags
+tags_metadata = [
+    {
+        "name": "Authentication",
+        "description": "Operations to register users and securely issue stateless **JWT Bearer Tokens**. Features timing-attack mitigation on login.",
+    },
+    {
+        "name": "User Management",
+        "description": "Admin-only routes to manage user roles and perform **soft-deletes** (deactivation) for audit compliance.",
+    },
+    {
+        "name": "Financial Records",
+        "description": "Core CRUD operations for the financial ledger. Includes a dynamic search engine with multi-parameter filtering.",
+    },
+    {
+        "name": "Analytics Dashboard",
+        "description": "High-performance, database-level SQL aggregations for real-time financial metrics and outlier detection.",
+    },
+]
+
+# 3. The Engine: Initializing the FastAPI Application
 app = FastAPI(
     title="Fintech Ledger API",
     description="Backend Screening Assessment",
     version="1.0.0",
+    openapi_tags=tags_metadata,
 )
 
-# 3. Plugging the Routers into the Application
+# 4. Plugging the Routers into the Application
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(records.router)
 app.include_router(analytics.router)
 
-# 4. A quick health check route to check how the server is doing
+# 5. A quick health check route to check how the server is doing
 @app.get("/")
 def read_root():
-    return {"status": "Fintech API Vault is Online & Healthy!"}
+    return {"status": "Fintech Ledger API Vault is Online & Healthy!"}
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Boolean, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from .database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
